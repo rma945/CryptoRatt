18618fe0b84d90a19e638ba3fa10ef2be1d8cb19
RomanCherednikov <cherednikov@saritasa.com>
2019-09-01 18:57:02 +0700

    Fix migrations

--- apps/ratticweb/urls.py
+++ apps/ratticweb/urls.py
@@ -1,8 +1,6 @@
-import django_saml2_auth.views
-import django_saml2_auth.urls
 from two_factor.urls import urlpatterns as django_twofactor_urls
 import django.contrib.admindocs.urls
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from tastypie.api import Api
 from apps.cred.api import CredResource, TagResource
@@ -27,24 +25,30 @@ v1_api.register(CredResource())
 v1_api.register(TagResource())
 v1_api.register(GroupResource())
 
-base_urlpatterns = []
+urlpatterns = []
 
-# SAML
-if settings.SAML_ENABLED:
-    base_urlpatterns += [
-        path('saml2_auth/', include(django_saml2_auth.urls)),
-        path('accounts/login/', django_saml2_auth.views.signin),
+# If debug mode - enable the Django admin interface for password reset
+if not settings.LDAP_ENABLED and not settings.SSO_ENABLED or settings.DEBUG:
+    from django.contrib import admin
+    urlpatterns += [
+        path('admin/', admin.site.urls)
+    ]
+
+# Enable SSO
+if settings.SSO_ENABLED:
+    urlpatterns += [
+        re_path(r'^oidc/', include('mozilla_django_oidc.urls')),
     ]
 
 # Setup the base paths for applications, and the API
-base_urlpatterns += [
+urlpatterns += [
     # Apps:
     path('', apps.ratticweb.views.home, name='home'),
     path('account/', include(apps.account.urls), name='account'),
     path('cred/', include(apps.cred.urls), name='cred'),
     path('staff/', include(apps.staff.urls), name='staff'),
     path('help/', include(apps.help.urls), name='help'),
-
+ 
     # API
     path('api/', include(v1_api.urls)),
 
@@ -58,30 +62,6 @@ base_urlpatterns += [
     path('', include(django_twofactor_urls)),
 ]
 
-# If debug mode - enable the Django admin interface
-if settings.DEBUG:
-    from django.contrib import admin
-    admin.autodiscover()
-    base_urlpatterns += [
-        path('admin/', admin.site.urls)
-    ]
-
-    if settings.SAML_ENABLED:
-        base_urlpatterns += [
-            path('admin/login/', django_saml2_auth.views.signin),
-        ]
-        
-
-# Strip any leading slash from the RATTIC_ROOT_URL
-if settings.RATTIC_ROOT_URL[0] == '/':
-    root = settings.RATTIC_ROOT_URL[1:]
-else:
-    root = settings.RATTIC_ROOT_URL
-
-# Serve RatticDB from an alternate root if requested
-urlpatterns = [
-    path('' + root, include(base_urlpatterns)),
-]
 
 # Serve the static files from the right location in dev mode
 if settings.DEBUG:
